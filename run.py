153db62f610a99f8f6b623c9cf91d75bb0956d53
ZueFe <ex3me@centrum.sk>
2018-05-28 21:35:21 +0200

    changing to chrome

--- run.py
+++ run.py
@@ -1,6 +1,17 @@
 import os
 import download as dw
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+GOOGLE_CHROME_BIN = '/app/.apt/usr/bin/google-chrome'
+CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+
+chrome_options = Options()
+chrome_options.binary_location = GOOGLE_CHROME_BIN
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--no-sandbox')
+driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
 
 
-dw.download_stats()
+#dw.download_stats()
 print(os.listdir(os.path.join('.', "data")))
