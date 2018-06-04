8d0630ea0e60f5fc9512c16e9ff6d009d548f89e
Zue <ex3me@centrum.sk>
2018-06-04 16:14:48 +0200

    removed headless option from chrome

--- download.py
+++ download.py
@@ -4,15 +4,19 @@ import constants as cs
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver import ChromeOptions
 from selenium.webdriver.support.ui import Select
+import time
 
 def setup_chrome_opt():
     chrome_options = ChromeOptions()
     chrome_options.binary_location = cs.GOOGLE_CHROME_BIN
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
-    prefs = {'download.default_directory' : cs.saveDir}
+    prefs = {'download.default_directory' : cs.saveDir,
+    "download.prompt_for_download": False,
+    "download.directory_upgrade": True,
+    "safebrowsing.enabled": True}
     chrome_options.add_experimental_option('prefs', prefs)
-    chrome_options.add_argument('--headless')
+    #chrome_options.add_argument('headless')
     driver = webdriver.Chrome(executable_path=cs.CHROMEDRIVER_PATH, chrome_options=chrome_options)
 
 
@@ -45,6 +49,9 @@ def download_stats(category = cs.CATEGORIES[0], team_stats = False, browser='chr
 
     elem = driver.find_elements_by_xpath("//*[contains(text(), 'Exportovat')]")[0]
     elem.click()
+
+    #Wait for the download
+    time.sleep(0.5)
 
     #TODO: better naming conventions for team/individual
     os.replace('{}stats.csv'.format(get_saveDir()), '{}stats_{}_{}.csv'.format(get_saveDir(), category, 'team' if team_stats else 'individual'))
