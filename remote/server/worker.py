50edf3995fdb7d2efea10d4e720ff5abce8c634b
Zue <ex3me@centrum.sk>
2018-07-14 12:51:03 +0200

    changed worker.py to download data bit earlier

--- remote/server/worker.py
+++ remote/server/worker.py
@@ -6,13 +6,13 @@ import scrape_data as sd
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=0, timezone='Europe/Bratislava')
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=23, minute=40 timezone='Europe/Bratislava')
 def work_data():
     """
     Schedules job, downloads all data from the page and stores them locally,
     then sends all downloaded data to remote FTP.
     Scheduled to run every day at midnight.
-    Uses *BlockingScheduler* class from *APScheduler* library.    
+    Uses *BlockingScheduler* class from *APScheduler* library.
     """
     print('Download started')
     sd.download_all()
